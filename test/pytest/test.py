5f0c957af1058785cd1271afb70ab9e7a161c756
Guy Korland <gkorland@gmail.com>
2019-10-27 16:15:38 +0200

    remove tests

--- test/pytest/test.py
+++ test/pytest/test.py
@@ -7,10 +7,10 @@ import unittest
 import json
 import os
 
-rmtest.config.REDIS_MODULE = os.path.abspath(os.path.join(os.getcwd(), 'target/debug/libredisx.so'))
+rmtest.config.REDIS_MODULE = os.path.abspath(os.path.join(os.getcwd(), 'target/debug/libredisrest.so'))
 
-class RedisXTestCase(BaseModuleTestCase):
-    """Tests RedisX Redis module in vitro"""
+class RedisRESTTestCase(BaseModuleTestCase):
+    """Tests RedisRET Redis module in vitro"""
 
     def assertNotExists(self, r, key, msg=None):
         self.assertFalse(r.exists(key), msg)
@@ -27,54 +27,6 @@ class RedisXTestCase(BaseModuleTestCase):
             r.client_setname(self._testMethodName)
             r.flushdb()
 
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.prepend', 'key')
-
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.prepend', 'key', 'abc', 'dfc')
-
-            self.assertEquals(3, r.execute_command('x.prepend', 'key1', 'abc'))
-            self.assertEquals('abc', r.get('key1'))
-
-            self.assertEquals(6, r.execute_command('x.prepend', 'key1', 'def'))
-            self.assertEquals('defabc', r.get('key1'))
-
-
-    def testGetEX(self):
-        """Test X.GETEX """
-        with self.redis() as r:
-            r.client_setname(self._testMethodName)
-            r.flushdb()
-
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.getex', 'key')
-
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.getex', 'key', 'a100')
-
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.getex', 'key', '100', 'dfc')
-
-            r.execute_command('set', 'key1', 'abc')
-            self.assertEquals('abc', r.execute_command('x.getex', 'key1', '100'))
-            self.assertEquals(100L, r.execute_command('ttl', 'key1'))
-            self.assertEquals(None, r.execute_command('x.getex', 'key2', '100'))
-
-    def testGetSetEX(self):
-        """Test X.GETSETEX """
-        with self.redis() as r:
-            r.client_setname(self._testMethodName)
-            r.flushdb()
-
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.getsetex', 'key', 'key', 'abc')
-
-            with self.assertRaises(redis.exceptions.ResponseError):
-                r.execute_command('x.getsetex', 'key', 'abc', 'a100')
-
-            self.assertEquals(None, r.execute_command('x.getsetex', 'key1', 'abc', 100))
-            self.assertEquals('abc', r.execute_command('x.getsetex', 'key1', 'def', 250))
-            self.assertEquals(250L, r.execute_command('ttl', 'key1'))
 
 if __name__ == '__main__':
     unittest.main()
