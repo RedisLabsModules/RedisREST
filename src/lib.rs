421ef0b576de911c19e5dc321db931feefa88ed7
Guy Korland <gkorland@gmail.com>
2019-10-29 13:41:54 +0200

    Added very simple Web CLI

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ extern crate redismodule;
 use redismodule::raw as rawmod;
 
 use redismodule::{Context, RedisValue};
+use rocket::response::content;
 use rocket::response::status::BadRequest;
 use std::path::PathBuf;
 use std::thread;
@@ -37,7 +38,7 @@ fn to_json<'a>(value: &'a RedisValue) -> String {
 }
 
 #[get("/<args..>")]
-fn index(args: PathBuf) -> Result<String, BadRequest<String>> {
+fn path(args: PathBuf) -> Result<String, BadRequest<String>> {
     let args: Vec<&str> = args.iter().map(|arg| arg.to_str().unwrap()).collect();
     let ctx = Context::get_thread_safe_context();
 
@@ -47,12 +48,46 @@ fn index(args: PathBuf) -> Result<String, BadRequest<String>> {
 
     match result {
         Ok(s) => Ok(to_json(&s)),
-        Err(e) => Err(BadRequest(Some(format!("{:?}", e)))), // TODO use formated e display
+        Err(e) => Err(BadRequest(Some(format!("{}", e)))),
     }
 }
 
+#[get("/")]
+fn index() -> content::Html<&'static str> {
+    content::Html(
+        "<html>
+            <head>
+                <script type=\"text/javascript\">
+                    function sendCmd(){
+                        let cmd = document.getElementById('cli').value;
+                        let xhr = new XMLHttpRequest();               
+                        xhr.open('GET', window.location.href + cmd.match(/\\w+|\"[^\"]+\"/g).join('/')); 
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState == XMLHttpRequest.DONE) {
+                                let result = document.createElement(\"LI\")
+                                let text = document.createTextNode(xhr.responseText);
+                                result.appendChild(text); 
+                                let results = document.getElementById('results');
+                                results.appendChild(result)
+                            }
+                        } 
+                        xhr.send();
+                    }
+                </script>
+            </head>
+            <body>
+                <form onsubmit=\"sendCmd();return false;\">
+                    Redis CLI: <input id=\"cli\" style=\"width: 60%;\"> <input type=\"submit\" value=\"Send\">
+                </form>
+                <ul id=\"results\">
+                </ul>
+            </body>
+        </html>",
+    )
+}
+
 pub extern "C" fn init(_raw_ctx: *mut rawmod::RedisModuleCtx) -> c_int {
-    thread::spawn(|| rocket::ignite().mount("/", routes![index]).launch());
+    thread::spawn(|| rocket::ignite().mount("/", routes![index, path]).launch());
     0
 }
 
